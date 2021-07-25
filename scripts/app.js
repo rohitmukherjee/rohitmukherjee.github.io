9edc65d3941c871743577d18737a721666850136
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2021-07-25 17:57:32 +0800

    Finished scene 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,4 @@
-function renderLegend(svg, continentKeys, width, myColor) {
-    // Add one dot in the legend for each name.
-    svg.selectAll("legend-dots")
-        .data(continentKeys)
-        .enter()
-        .append("circle")
-        .attr("cx", width - 100)
-        .attr("cy", function (d, i) {
-            return 50 + i * 25
-        }) // 100 is where the first dot appears. 25 is the distance between dots
-        .attr("r", 2)
-        .style("fill", function (d) {
-            return myColor(d)
-        })
-
-    svg.selectAll("legend-labels")
-        .data(continentKeys)
-        .enter()
-        .append("text")
-        .attr("x", width + 8 - 100)
-        .attr("y", function (d, i) {
-            return 50 + i * 25
-        }) // 100 is where the first dot appears. 25 is the distance between dots
-        .style("fill", function (d) {
-            return myColor(d)
-        })
-        .text(function (d) {
-            return d
-        })
-        .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
-}
-
+// First Slide
 async function renderFirstChart() {
     const margin = {top: 10, right: 20, bottom: 30, left: 50},
         width = 1000 - margin.left - margin.right,
@@ -41,7 +9,6 @@ async function renderFirstChart() {
         return d.year == year && d.total_population != "" && d.average_annual_hours_worked != "" && d.gdp_per_capita != "";
     });
 
-    console.log(filteredData);
     let svg = d3.select("#chart-1").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -122,14 +89,7 @@ function firstChartTooltipHTML(object) {
     return "<div>Country: " + object.entity + "</div><div>Population: " + object.total_population + "</div><div>GDP per capita: $" + object.gdp_per_capita + "</div>";
 }
 
-function getBubbleSizeScale() {
-    // Add a scale for bubble size
-    const z = d3.scaleLog()
-        .domain([200000, 1310000000])
-        .range([1, 30]);
-    return z;
-}
-
+// Second Slide
 async function renderSecondChart() {
     const margin = {top: 10, right: 20, bottom: 30, left: 50},
         width = 800 - margin.left - margin.right,
@@ -221,6 +181,169 @@ function secondChartTooltipHTML(object) {
     return "<div>Country: " + object.entity + "</div><div>Population: " + object.total_population + "</div><div>Productivity: $" + object.productivity + "\/hour</div>";
 }
 
+// Third Slide
+
+
+async function renderThirdChart() {
+    const margin = {top: 10, right: 20, bottom: 30, left: 50},
+        width = 800 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+    const data = await d3.csv("https://rohitmukherjee.github.io/data/4-labor-productivity-per-hour-PennWorldTable.csv");
+    // append the svg object to the body of the page
+    const svg = d3.select("#chart-3")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    const filteredData = data.filter(function (d) {
+        return d.productivity != "" && d.year != "";
+    });
+
+    const entities = getEntities();
+    d3.select("#select-country")
+        .selectAll('country-options')
+        .data(entities)
+        .enter()
+        .append('option')
+        .text(function (d) {
+            return d;
+        }) // text showed in the menu
+        .attr("value", function (d) {
+            return d;
+        }) // corresponding value returned by the button
+
+
+    // A color scale: one color for each group
+    const myColor = d3.scaleOrdinal()
+        .domain(entities)
+        .range(d3.schemeSet2);
+
+    // Add X axis to measure time
+    const x = d3.scaleLinear()
+        .domain([1950, 2017])
+        .range([0, width]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Initialize line with group a
+    const line = svg
+        .append('g')
+        .append("path")
+        .datum(filteredData.filter(function (d) {
+            return d.entity === entities[0];
+        }))
+        .attr("d", d3.line()
+            .x(function (d) {
+                return x(Number(d.year))
+            })
+            .y(function (d) {
+                return y(Number(d.productivity))
+            })
+        )
+        .attr("stroke", function (d) {
+            return myColor(d.entity)
+        })
+        .style("stroke-width", 4)
+        .style("fill", "none")
+
+    function update(selectedGroup) {
+        // Create new data with the selection?
+        const dataFilter = filteredData.filter(function (d) {
+
+            return d.entity === selectedGroup;
+        });
+
+        // Give these new data to update line
+        line
+            .datum(dataFilter)
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+                .x(function (d) {
+                    return x(Number(d.year))
+                })
+                .y(function (d) {
+                    return y(Number(d.productivity))
+                })
+            )
+            .attr("stroke", function (d) {
+                return myColor(selectedGroup)
+            })
+    }
+
+    // When the button is changed, run the updateChart function
+    d3.select("#select-country").on("change", function (d) {
+        // recover the option that has been chosen
+        const selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    })
+
+}
+
+
+// Common functions
+function getBubbleSizeScale() {
+    // Add a scale for bubble size
+    const z = d3.scaleLog()
+        .domain([200000, 1310000000])
+        .range([1, 30]);
+    return z;
+}
+
+function renderLegend(svg, continentKeys, width, myColor) {
+    // Add one dot in the legend for each name.
+    svg.selectAll("legend-dots")
+        .data(continentKeys)
+        .enter()
+        .append("circle")
+        .attr("cx", width - 100)
+        .attr("cy", function (d, i) {
+            return 50 + i * 25
+        }) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("r", 2)
+        .style("fill", function (d) {
+            return myColor(d)
+        })
+
+    svg.selectAll("legend-labels")
+        .data(continentKeys)
+        .enter()
+        .append("text")
+        .attr("x", width + 8 - 100)
+        .attr("y", function (d, i) {
+            return 50 + i * 25
+        }) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function (d) {
+            return myColor(d)
+        })
+        .text(function (d) {
+            return d
+        })
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+}
+
 function getContinentKeys() {
     return ["Asia", "Europe", "North America", "South America", "Africa", "Oceania"];
 }
+
+function getEntities() {
+    return ["Argentina", "Australia", "Austria", "Bangladesh", "Barbados", "Belgium", "Brazil", "Bulgaria", "Cambodia", "Canada", "Chile", "China",
+        "Colombia", "Costa Rica", "Croatia", "Cyprus", "Czechia", "Denmark", "Ecuador", "Estonia", "Finland", "France", "Germany", "Greece", "Hong Kong",
+        "Hungary", "Iceland", "India", "Indonesia", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Latvia", "Lithuania", "Luxembourg", "Malaysia", "Malta",
+        "Mexico", "Myanmar", "Netherlands", "New Zealand", "Nigeria", "Norway", "Pakistan", "Peru", "Philippines", "Poland", "Portugal", "Romania", "Russia",
+        "Saint Lucia", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka",
+        "Sweden", "Switzerland", "Taiwan", "Thailand", "Trinidad and Tobago", "Turkey", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam"]
+}
+
+
