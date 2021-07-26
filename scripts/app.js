46824cc11d342318a5515b9383421057cb793eaf
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2021-07-26 16:26:42 +0800

    Added choropleth

--- scripts/app.js
+++ scripts/app.js
@@ -290,6 +290,52 @@ async function renderThirdChart() {
 
 }
 
+// Fourth slide
+function renderFourthChart() {
+    // The svg
+    const svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+// Map and projection
+    const path = d3.geoPath();
+    const projection = d3.geoMercator()
+        .scale(70)
+        .center([0, 20])
+        .translate([width / 2, height / 2]);
+
+// Data and color scale
+    let data = new Map()
+    const colorScale = d3.scaleOrdinal()
+        .domain(getContinentKeys())
+        .range(d3.schemeOranges[9]);
+
+// Load external data and boot
+    Promise.all([
+        d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+        d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function (d) {
+            data.set(d.code, +d.pop)
+        })
+    ]).then(function (loadData) {
+        let topo = loadData[0]
+
+        // Draw the map
+        svg.append("g")
+            .selectAll("path")
+            .data(topo.features)
+            .join("path")
+            // draw each country
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+            // set the color of each country
+            .attr("fill", function (d) {
+                d.total = data.get(d.id) || 0;
+                return colorScale(d.total);
+            })
+    })
+
+}
 
 // Common functions
 function getBubbleSizeScale() {
